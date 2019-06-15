a2c51942d316db79735d89fb1d02aa6fd84d638f
Catherine Weinberg <catherinewweinberg@gmail.com>
2019-06-14 22:58:04 -0400

    added font style in css page and added background color

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,44 +1,71 @@
-
-
-
-var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y","z"];
+var alphabet = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z"
+];
 var guessesNum = 9;
 var guessChoice = [];
 var wins = 0;
 var losses = 0;
 
+var displayResults = function() {
+  document.getElementById("wins").innerHTML = "Wins: " + wins;
+  document.getElementById("losses").innerHTML = "Losses: " + losses;
+  document.getElementById("guessesNum").innerHTML =
+    "Guesses Left: " + guessesNum;
+  document.getElementById("guessChoice").innerHTML =
+    "Your guesses so far: " + guessChoice;
+};
+var reset = function() {
+  guessChoice = [];
+  guessesNum = 9;
+  computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
+};
+
+var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
+
 document.onkeyup = function(event) {
-    var userGuess = event.key;
-    guessChoice.push(userGuess);
+  var userGuess = event.key;
+  guessChoice.push(userGuess);
 
-    var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
-        console.log(computerChoice);
+  console.log(computerChoice);
 
+  if (userGuess === computerChoice) {
+    wins++;
+    reset();
+    alert("Way to go!");
+  } else {
+    guessesNum--;
+  }
 
-    if (userGuess === computerChoice) {
-        wins++;
-        guessesNum = 9;
-        alert("Way to go!");
-    }else{
-        guessesNum--;
-    }
-    if (guessChoice !== computerChoice) { 
-        losses++;
-    }
-    
+  if (guessesNum <= 0) {
+    losses++;
+    reset();
+    alert("Sorry Try Again!");
+  }
 
-// function win()    
-// wins.innerHTML = wins;
-// losses.innerHTML = losses;
-// }
-// function wins() {
-//     wins++;
-//     resetGame();
-//     }
-    document.getElementById("wins").innerHTML = "Wins: " + wins;
-    document.getElementById("losses").innerHTML = "Losses: " + losses;
-    document.getElementById("guessesNum").innerHTML = "Guesses Left: " + guessesNum; 
-    document.getElementById("guessChoice").innerHTML = "Your guesses so far: " + guessChoice;
-    document.getElementById("guessNum = 0").reset();
-
-    }
+  displayResults();
+};
